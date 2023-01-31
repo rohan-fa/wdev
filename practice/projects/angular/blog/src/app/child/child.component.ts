56113371bfd89036782569579cf0ad3a631cdfed
rohan -fa <rohanfarzna@gmail.com>
2023-01-31 19:22:38 +1100

    angular basics

---
+++ practice/projects/angular/blog/src/app/child/child.component.ts
@@ -0,0 +1,10 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent {
+@ Output() rohanDataEvent = new EventEmitter<string>();
+}
